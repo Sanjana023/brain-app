d7e75a18d07e046119a63b0bc29dae50f9d56a1d
Sanjana023 <duttasanjana237@gmail.com>
2025-07-16 19:15:17 +0530

    frontend started(TopBar)

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import TopBar from "../components/Topbar";
+
+const HomePage = () => {
+ return (
+    <div>
+      <TopBar />
+      {/* your notes/cards layout here */}
+    </div>
+  );
+
+}
+
+export default HomePage
